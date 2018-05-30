7effea94a76aa9a0a2c500dca3d1f0ae5a0ae131
Gaurav Padawe <grvpdw92@gmail.com>
2018-05-30 20:56:13 +0530

    Initial commit

---
+++ Eligibility.py
@@ -0,0 +1,16 @@
+﻿age = ""
+name = ""
+
+def eligibility(name,age):
+	while True:
+		name = input("Customer Name: ")
+		try:
+			age = int(input("Age: "))
+			if age >= 18:
+				print "Allowed"
+			else:
+				print "Sorry"
+		except:
+			print "Note: Please insert your age in numbers"
+			
+print eligibility(name,age)
